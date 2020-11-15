6f03090932bf41fd7ddf66bcb8818d0aa498811d
pmayur <thisispatil@gmail.com>
2020-11-15 20:56:45 +0530

    WIP

--- lib/ui/authentication.dart
+++ lib/ui/authentication.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:wallet/net/flutterfire.dart';
+
+import 'home_view.dart';
 
 class Authentication extends StatefulWidget {
   Authentication({Key key}) : super(key: key);
@@ -8,7 +11,6 @@ class Authentication extends StatefulWidget {
 }
 
 class _AuthenticationState extends State<Authentication> {
-  /* Controllers for EMAIL and PASSWORD fields */
   TextEditingController _emailField = TextEditingController();
   TextEditingController _passwordField = TextEditingController();
 
@@ -18,66 +20,94 @@ class _AuthenticationState extends State<Authentication> {
       body: Container(
         width: MediaQuery.of(context).size.width,
         height: MediaQuery.of(context).size.height,
-        decoration: BoxDecoration(color: Colors.cyanAccent),
+        decoration: BoxDecoration(
+          color: Colors.blueAccent,
+        ),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-
-            /* EMAIL FIELD */
-            TextFormField(
-              controller: _emailField,
-              decoration: InputDecoration(
-                hintText: "youremail@email.com",
-                hintStyle: TextStyle(
-                  color: Colors.white,
-                ),
-                labelText: "Email",
-                labelStyle: TextStyle(
-                  color: Colors.white,
-                ),
-              ),
-            ),
-
-            /* PASSWORD FIELD */
-            TextFormField(
-              controller: _passwordField,
-              obscureText: true,
-              decoration: InputDecoration(
-                hintText: "password",
-                hintStyle: TextStyle(
-                  color: Colors.white,
-                ),
-                labelText: "Password",
-                labelStyle: TextStyle(
-                  color: Colors.white,
+            Container(
+              width: MediaQuery.of(context).size.width / 1.3,
+              child: TextFormField(
+                style: TextStyle(color: Colors.white),
+                controller: _emailField,
+                decoration: InputDecoration(
+                  hintText: "something@email.com",
+                  hintStyle: TextStyle(
+                    color: Colors.white,
+                  ),
+                  labelText: "Email",
+                  labelStyle: TextStyle(
+                    color: Colors.white,
+                  ),
                 ),
               ),
             ),
-
-            /* REGISTER BUTTON */
+            SizedBox(height: MediaQuery.of(context).size.height / 35),
+            Container(
+              width: MediaQuery.of(context).size.width / 1.3,
+              child: TextFormField(
+                style: TextStyle(color: Colors.white),
+                controller: _passwordField,
+                obscureText: true,
+                decoration: InputDecoration(
+                  hintText: "password",
+                  hintStyle: TextStyle(
+                    color: Colors.white,
+                  ),
+                  labelText: "Password",
+                  labelStyle: TextStyle(
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(height: MediaQuery.of(context).size.height / 35),
             Container(
               width: MediaQuery.of(context).size.width / 1.4,
               height: 45,
               decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(15), color: Colors.white,
+                borderRadius: BorderRadius.circular(15.0),
+                color: Colors.white,
               ),
               child: MaterialButton(
-                onPressed: () {},
+                onPressed: () async {
+                  bool shouldNavigate =
+                      await register(_emailField.text, _passwordField.text);
+                  if (shouldNavigate) {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => HomeView(),
+                      ),
+                    );
+                  }
+                },
                 child: Text("Register"),
               ),
             ),
-
-            /* LOGIN BUTTON */
+            SizedBox(height: MediaQuery.of(context).size.height / 35),
             Container(
               width: MediaQuery.of(context).size.width / 1.4,
               height: 45,
               decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(15), color: Colors.white,
+                borderRadius: BorderRadius.circular(15.0),
+                color: Colors.white,
               ),
               child: MaterialButton(
-                onPressed: () {},
-                child: Text("Log In"),
-              ),
+                  onPressed: () async {
+                    bool shouldNavigate =
+                        await signIn(_emailField.text, _passwordField.text);
+                    if (shouldNavigate) {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => HomeView(),
+                        ),
+                      );
+                    }
+                  },
+                  child: Text("Login")),
             ),
           ],
         ),
