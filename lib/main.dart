99624b9236bdaedd741749009a8f2f501402bc13
pmayur <thisispatil@gmail.com>
2020-11-19 00:12:02 +0530

    Remove debug banner / transparent notification bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:wallet/ui/authentication.dart';
+import 'package:flutter/services.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -12,9 +13,13 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+      statusBarColor: Colors.transparent,
+    ));
     return MaterialApp(
       title: 'Wallet',
       home: Authentication(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
